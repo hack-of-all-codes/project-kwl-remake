c16b4e205b75b93d1c5365610c2b785207471d35
LM Bibera <lyndon.bibera@boltoninternational.com>
2017-07-18 07:55:57 +0800

    poc: generated template project for running tika

---
+++ POC/tika-poc/src/main/java/com/kwl2/poc/tika/TikaPocApplication.java
@@ -0,0 +1,12 @@
+package com.kwl2.poc.tika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikaPocApplication.class, args);
+	}
+}
