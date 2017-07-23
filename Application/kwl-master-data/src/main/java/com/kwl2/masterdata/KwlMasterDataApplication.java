648a2cf30fe7a32e42d7905ec90ed9a1b4d02ef6
LM Bibera <secret.transaction@gmail.com>
2017-07-23 17:30:04 +0800

    generated master data project

---
+++ Application/kwl-master-data/src/main/java/com/kwl2/masterdata/KwlMasterDataApplication.java
@@ -0,0 +1,12 @@
+package com.kwl2.masterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwlMasterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwlMasterDataApplication.class, args);
+	}
+}
