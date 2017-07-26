2ff69323f4be947b0c38efb036f00530a1c77402
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-07-26 14:04:33 +0800

    generated POC project for testing out livy

---
+++ POC/livy-poc/src/main/java/com/kwl2/poc/livy/LivyPocApplication.java
@@ -0,0 +1,12 @@
+package com.kwl2.poc.livy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivyPocApplication.class, args);
+	}
+}
---
+++ POC/livy-poc/src/test/java/com/kwl2/poc/livy/LivyPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kwl2.poc.livy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LivyPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
