536e3d9bdc45530554ada4e5dcda4c9fdc3689a5
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-07-31 18:38:31 +0800

    temp: build takes so long. will continues this at home
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

--- POC/livy-poc/src/main/java/com/kwl2/poc/livy/LivyPocApplication.java
+++ POC/livy-poc/src/main/java/com/kwl2/poc/livy/LivyPocApplication.java
@@ -1,7 +1,17 @@
 package com.kwl2.poc.livy;
 
+import com.cloudera.livy.LivyClient;
+import com.cloudera.livy.LivyClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 //livy docs: https://github.com/apache/incubator-livy#rest-api
 @SpringBootApplication
@@ -10,4 +20,17 @@ public class LivyPocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LivyPocApplication.class, args);
 	}
+
+	@Bean
+	LivyClient livyClient(@Value("livy.host") String livyHost) throws IOException, URISyntaxException {
+		return new LivyClientBuilder().setURI(new URI(livyHost)).build();
+	}
+
+	@Bean
+	CommandLineRunner runner(@Autowired LivyClient livyClient) {
+	    return (args) -> {
+	        double pi = livyClient.submit(new PiJob(10)).get();
+	        System.out.println("Pi is:" + pi);
+        };
+    }
 }
