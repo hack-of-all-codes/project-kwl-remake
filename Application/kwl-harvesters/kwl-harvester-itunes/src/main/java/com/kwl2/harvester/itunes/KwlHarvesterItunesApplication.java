86e5e73eebc169d24c75232b91107df571b4e80b
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-08-02 16:33:09 +0800

    generated microservice for harvesting itunes data
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

---
+++ Application/kwl-harvesters/kwl-harvester-itunes/src/main/java/com/kwl2/harvester/itunes/KwlHarvesterItunesApplication.java
@@ -0,0 +1,12 @@
+package com.kwl2.harvester.itunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwlHarvesterItunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwlHarvesterItunesApplication.class, args);
+	}
+}
