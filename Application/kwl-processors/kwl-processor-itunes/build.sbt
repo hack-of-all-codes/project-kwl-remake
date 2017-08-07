0cee01a617c24b49372e2eecdf9cf2794496ee74
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-08-07 12:36:40 +0800

    ISSUE#2: partially implemented spark job for moving itunes EPF artist to postgresql
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

--- Application/kwl-processors/kwl-processor-itunes/build.sbt
+++ Application/kwl-processors/kwl-processor-itunes/build.sbt
@@ -7,7 +7,8 @@ scalaVersion := "2.11.11"
 libraryDependencies ++= {
   val sparkVer = "2.1.1"
   Seq(
-    "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources()
+    "org.apache.spark" %% "spark-core" % sparkVer % "provided" withSources(),
+    "org.apache.spark" %% "spark-sql" % sparkVer % "provided" withSources()
   )
 }
 
--- Application/kwl-processors/kwl-processor-itunes/src/main/scala/com/kwl2/harvester/itunes/artist/ArtistProcessor.scala
+++ Application/kwl-processors/kwl-processor-itunes/src/main/scala/com/kwl2/harvester/itunes/artist/ArtistProcessor.scala
@@ -1,11 +1,23 @@
 package com.kwl2.harvester.itunes.artist
 
+import org.apache.spark.sql.SparkSession
+
 /**
   * Loads artist file from HDFS to Kafka
   */
 object ArtistProcessor {
 
   def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Artist Processor V1")
+      .getOrCreate()
+
+    val artistItunesHDFS = spark.read.textFile(args(0)).filter(!_.startsWith("#"))
+    artistItunesHDFS.show()
+
+    val artistItunesJDBC = spark.read.jdbc(args(0), "itunes_artist", new java.util.Properties())
+    artistItunesJDBC.show()
   }
 
 }
