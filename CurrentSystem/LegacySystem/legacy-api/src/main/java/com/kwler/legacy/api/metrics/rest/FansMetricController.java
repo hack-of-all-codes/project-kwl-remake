7d033ac68a86c892c212fb8bbec37c520e28b42e
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-09-14 17:08:59 +0800

    code cleanup: removed clutter

--- CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/metrics/rest/FansMetricController.java
+++ CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/metrics/rest/FansMetricController.java
@@ -6,16 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("metric/fans")
 public class FansMetricController {
 
@@ -23,7 +20,6 @@ public class FansMetricController {
     FactFansMapper factFansMapper;
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public PagedResources<FactFans> showAll() {
         Collection<FactFans> fansmetrics = factFansMapper.findByDate(20170809, 20);
 
@@ -33,7 +29,6 @@ public class FansMetricController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "single")
-    @ResponseBody
     public Resource<FactFans> showOne() {
         FactFans f = factFansMapper.findByDate(20170809, 10).get(0);
         return new Resource<>(f, new Link("http://localhost:17081"));
