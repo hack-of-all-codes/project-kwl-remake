ff2c87bf67d54970be14940442ed92e70679fa33
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-09-20 13:43:41 +0800

    added demonstration on MyBatis resultMap
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

--- CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/metrics/mapper/FactFansMapper.java
+++ CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/metrics/mapper/FactFansMapper.java
@@ -1,11 +1,11 @@
 package com.kwler.legacy.api.metrics.mapper;
 
 import com.kwler.legacy.api.metrics.model.FactFans;
-import com.kwler.legacy.api.metrics.model.ArtistResults;
-import org.apache.ibatis.annotations.*;
-import org.springframework.data.domain.Page;
+import com.kwler.legacy.api.metrics.model.PartialResult;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.PagedResources;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,9 +21,7 @@ public interface FactFansMapper {
     @Select("SELECT * FROM fact_fans WHERE date_id = #{date} LIMIT #{limit}")
     List<Map<String, Object>> findByDateM(@Param("date") Integer date, @Param("limit") Integer limit);
 
-    ArtistResults findArtists(@Param("limit") Integer limit);
-
-    List<FactFans> findFansByDateAndArtistId(
+    PartialResult<FactFans> findFansByDateAndArtistId(
             @Param("date") Integer date,
             @Param("artist") String artistId,
             @Param("page") Pageable page);
