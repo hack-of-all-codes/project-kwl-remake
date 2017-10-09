c73bcef65fb7995fe2f4c2c1f343cacd1996b2bc
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-10-09 12:58:24 +0800

    generated kwl-scheduler project

---
+++ CurrentSystem/LegacySystem/legacy-scheduler/src/main/java/com/kwler/legacy/scheduler/LegacySchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.kwler.legacy.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacySchedulerApplication.class, args);
+	}
+}
