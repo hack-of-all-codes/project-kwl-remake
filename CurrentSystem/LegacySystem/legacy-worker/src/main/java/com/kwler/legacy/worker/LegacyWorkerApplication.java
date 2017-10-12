88d233968e74fbfc8259663215e22b6b728e5f7c
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-10-13 06:51:04 +0800

    generated worker project
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

---
+++ CurrentSystem/LegacySystem/legacy-worker/src/main/java/com/kwler/legacy/worker/LegacyWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.kwler.legacy.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyWorkerApplication.class, args);
+	}
+}
