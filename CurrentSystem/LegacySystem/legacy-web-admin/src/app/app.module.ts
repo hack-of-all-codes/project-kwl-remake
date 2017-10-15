4c9356d26d4157dad607e0da36603218500c9de6
LM Bibera <secret.transaction@gmail.com>
2017-10-15 18:41:18 +0800

    initial port: login page plus fonts and styles

--- CurrentSystem/LegacySystem/legacy-web-admin/src/app/app.module.ts
+++ CurrentSystem/LegacySystem/legacy-web-admin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
