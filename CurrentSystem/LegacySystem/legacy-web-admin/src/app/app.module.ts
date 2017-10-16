a23a992b40db0e5c9ba3d1b96b7e8d455262cd36
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-10-16 09:33:32 +0800

    partial implementation of the login form: started with some routers and added the forms module

--- CurrentSystem/LegacySystem/legacy-web-admin/src/app/app.module.ts
+++ CurrentSystem/LegacySystem/legacy-web-admin/src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {PasswordRecoveryComponent} from './components/password-recovery/password-recovery.component';
+import {AdminDashboardComponent} from './components/admin-dashboard/admin-dashboard.component';
+
+const appRoutes: Routes = [
+  {path: 'admin', component: AdminDashboardComponent},
+  {path: 'auth/login', component: LoginComponent},
+  {path: 'auth/password-recovery', component: PasswordRecoveryComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    PasswordRecoveryComponent,
+    AdminDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- CurrentSystem/LegacySystem/legacy-web-admin/src/app/components/login/login.component.ts
+++ CurrentSystem/LegacySystem/legacy-web-admin/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "./user";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = new User("test@test.com", "password", true);
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.user.email = "test@test.com";
   }
 
 }
