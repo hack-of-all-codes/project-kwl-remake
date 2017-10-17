af236ab5c1c19c57d6047b3b47b9db837a6ffe3f
LM Bibera <secret.transaction@gmail.com>
2017-10-17 17:51:03 +0800

    added cors

--- CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/WebSecurityConfig.java
+++ CurrentSystem/LegacySystem/legacy-api/src/main/java/com/kwler/legacy/api/WebSecurityConfig.java
@@ -2,11 +2,10 @@ package com.kwler.legacy.api;
 
 import com.kwler.legacy.api.security.BCryptPasswordEncoder;
 import com.kwler.legacy.api.security.StandardUserDetailsService;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,16 +13,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, mode = AdviceMode.ASPECTJ)
-@FieldDefaults(makeFinal=true, level= AccessLevel.PRIVATE)
-@AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     StandardUserDetailsService standardUserDetailsService;
+
+    @Autowired
     BCryptPasswordEncoder passwordEncoder;
+
+    @Value("${app.cors.allowed.origins}")
+    String[] allowedOrigins;
+
+    @Value("${app.cors.allowed.methods}")
+    String[] allowedMethods;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -39,14 +50,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
 
                 .and().httpBasic()
+                .and().cors()
                 .and().csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(standardUserDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedMethods(Arrays.asList(allowedMethods));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
